88000229a3e3df44621b45674a3aae212b8c1e6c
jong-k <fully.charged07@gmail.com>
2025-11-05 15:27:23 +0900

    feat: background color classname props to top banner

--- src/widgets/header/ui/top-banner.tsx
+++ src/widgets/header/ui/top-banner.tsx
@@ -1,12 +1,17 @@
 import { Megaphone } from "lucide-react";
+import { cn } from "@/shared/shadcn-ui/lib/utils";
 
 interface TopBannerProps {
   text: string;
+  backgroundColorClassname?: string;
 }
 
-export default function TopBanner({ text }: TopBannerProps) {
+export default function TopBanner({
+  text,
+  backgroundColorClassname = "bg-[linear-gradient(114.41deg,#0ae448_20.74%,#abff84_65.5%)]",
+}: TopBannerProps) {
   return (
-    <div className="flex h-top-header w-full items-center justify-center bg-[linear-gradient(114.41deg,#0ae448_20.74%,#abff84_65.5%)]">
+    <div className={cn("flex h-top-banner w-full items-center justify-center", backgroundColorClassname)}>
       <Megaphone size="20" className="mr-2 inline-block" />
       <span className="text-sm">{text}</span>
     </div>
