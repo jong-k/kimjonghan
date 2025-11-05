9eda27d43f3bb365dcd8f68e48723ca39bbde800
jong-k <fully.charged07@gmail.com>
2025-11-05 22:00:51 +0900

    chore: add no-layout /hidden page

--- src/app/(default-layout)/layout.tsx
+++ src/app/(default-layout)/layout.tsx
@@ -1,5 +1,5 @@
+import "@/styles/globals.css";
 import { Header } from "@/widgets/header/ui";
-import "../styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
---
+++ src/app/(no-layout)/hidden/page.tsx
@@ -0,0 +1,3 @@
+export default function HiddenPage() {
+  return <div>Hidden Page</div>;
+}
