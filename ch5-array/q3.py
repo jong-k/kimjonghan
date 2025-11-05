23bdfe8e24aafcdded59a783cce3dbffab0f5881
jong-k <fully.charged07@gmail.com>
2025-11-05 09:57:59 +0900

    pivot project

--- ch5-array/q3.py
+++
@@ -1,8 +0,0 @@
-# 프로그래머스 두 개 뽑아서 더하기
-def solution(numbers):
-    s1 = set()
-    length = len(numbers)
-    for i in range(length):
-        for j in range(i + 1, length):
-            s1.add(numbers[i] + numbers[j])
-    return sorted(list(s1))
