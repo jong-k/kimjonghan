243570375128261ccdc9cfbb25b6728c60685a1b
jong-k <fully.charged07@gmail.com>
2025-11-09 17:18:51 +0900

    refactor: remove unnecessary code

--- src/widgets/header/model/use-prefix.ts
+++ src/widgets/header/model/use-prefix.ts
@@ -10,16 +10,10 @@ const DEFAULT_PREFIX = Object.freeze({
 
 export const usePrefix = () => {
   const [prefix, setPrefix] = useState<Prefix>(DEFAULT_PREFIX);
-  const [hasPrefix, setHasPrefix] = useState(false);
-
-  const resetPrefix = () => {
-    setPrefix(DEFAULT_PREFIX);
-    setHasPrefix(false);
-  };
 
   const generatePrefix = () => {
-    if (hasPrefix) return;
-    setHasPrefix(true);
+    if (prefix.name !== "" || prefix.title !== "") return;
+
     const chosenPrefixes = pickRandomItem(PREFIX_GROUPS);
 
     if (chosenPrefixes.category === "name") {
@@ -34,10 +28,10 @@ export const usePrefix = () => {
   useEffect(() => {
     if (prefix.name === "" && prefix.title === "") return;
     const timer = setTimeout(() => {
-      resetPrefix();
+      setPrefix(DEFAULT_PREFIX);
     }, 1250);
     return () => clearTimeout(timer);
   }, [prefix]);
 
-  return { prefix, generatePrefix, resetPrefix };
+  return { prefix, generatePrefix };
 };
